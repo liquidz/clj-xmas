8ef9f122d7837a7b3aee6099846f178864404d43
liquidz <liquidz.uo@gmail.com>
2021-12-18 06:20:17 +0900

    refactor: Extract core/run which is a core function to print tree

--- src/xmas/core.clj
+++ src/xmas/core.clj
@@ -23,6 +23,13 @@
               \u0020 \u2E1B \u2042 \u2E2E "&" "@" \uFF61])
 (def object-colors [21 33 34 35 36 37])
 
+(def cli-options
+  [["-s" "--size SIZE" "Tree size" :default 5 :parse-fn parse-long :validate [#(< 0 % 100)]]
+   ["-n" "--number NUMBER" "Number of trees" :default 1 :parse-fn parse-long]
+   ["-a" "--animation"]
+   ["-i" "--interval INTERVAL" :default 1 :parse-fn parse-long]
+   ["-h" "--help"]])
+
 (defn generate-tree [^Random r size]
   (concat
    [(strs (inc size) " " star (strs (inc size) " "))]
@@ -39,27 +46,23 @@
   (doseq [line (apply map #(str/join " " %&) (repeatedly n #(generate-tree r size)))]
     (println line)))
 
-(def cli-options
-  [["-s" "--size SIZE" "Tree size" :default 5 :parse-fn parse-long :validate [#(< 0 % 100)]]
-   ["-n" "--number NUMBER" "Number of trees" :default 1 :parse-fn parse-long]
-   ["-a" "--animation"]
-   ["-i" "--interval INTERVAL" :default 1 :parse-fn parse-long]
-   ["-h" "--help"]])
-
-(defn -main
-  [& args]
-  (let [{:keys [options summary errors]} (cli/parse-opts args cli-options)
-        {:keys [size number animation interval help]} options
+(defn run [options]
+  (let [default-options (:options (cli/parse-opts [] cli-options))
+        {:keys [size number animation interval]} (merge default-options options)
         r (Random. (System/currentTimeMillis))]
-    (cond
-      errors (doseq [e errors] (println e))
-      help (println (str "Usage:\n" summary))
-
-      animation
+    (if animation
       (while true
         (print-tree r size number)
         (cursor-up (+ 3 size))
         (Thread/sleep (* interval 1000)))
+      (print-tree r size number))))
+
+(defn -main
+  [& args]
+  (let [{:keys [options summary errors]} (cli/parse-opts args cli-options)]
+    (cond
+      errors (doseq [e errors] (println e))
+      (:help options) (println (str "Usage:\n" summary))
 
       :else
-      (print-tree r size number))))
+      (run options))))
